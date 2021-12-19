0a984ee36d02a939bf976207109ba644b0cdbfcb
jojoavila <avilajosuealejandro@gmail.com>
2021-12-19 15:50:32 -0700

    scrip base

--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using LaMancha.Services;
+using LaMancha.Casting;
+using LaMancha.Scripting;
 
-namespace LaMancha_
+
+namespace LaMancha
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create the cast
+            Dictionary<string, List<Actor>> cast = new Dictionary<string, List<Actor>>();
+
+            // Bricks
+            cast["player1"] = new List<Actor>();
+
+            // TODO: Add your bricks here
+
+            // The Ball (or balls if desired)
+            cast["player2"] = new List<Actor>();
+
+            // TODO: Add your ball here
+
+            // The paddle
+            cast["platfrm"] = new List<Actor>();
+
+            // TODO: Add your paddle here
+
+            // Create the script
+            Dictionary<string, List<Action>> script = new Dictionary<string, List<Action>>();
+
+            OutputService outputService = new OutputService();
+            InputService inputService = new InputService();
+            PhysicsService physicsService = new PhysicsService();
+            AudioService audioService = new AudioService();
+
+            script["output"] = new List<Action>();
+            script["input"] = new List<Action>();
+            script["update"] = new List<Action>();
+
+            DrawActorsAction drawActorsAction = new DrawActorsAction(outputService);
+            script["output"].Add(drawActorsAction);
+
+            // TODO: Add additional actions here to handle the input, move the actors, handle collisions, etc.
+
+            // Start up the game
+            outputService.OpenWindow(Constants.MAX_X, Constants.MAX_Y, "Batter", Constants.FRAME_RATE);
+            audioService.StartAudio();
+            audioService.PlaySound(Constants.SOUND_START);
+
+            Director theDirector = new Director(cast, script);
+            theDirector.Direct();
+
+            audioService.StopAudio();
         }
     }
 }
